532a5317017b76545f0fdbbd7c0197e99aad612b
Suhas <suhaskn09@gmail.com>
2019-08-09 11:04:40 +0200

    next

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const Sse = require('json-sse')
 const bodyParser = require('body-parser')
 
-const data = 'hello world'
+const messages = ['hello world']
 
-const sse = new Sse(data)
+const sse = new Sse(messages)
 
 const app = express()
 
@@ -15,10 +15,44 @@ app.get('/stream', sse.init)
 
 app.post('/message', (req,res) => {
   const { message } = req.body
+  messages.push(message)
+
+  sse.updateInit(message)
   sse.send(message)
+
+
   res.send(message)
 })
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Listning on ${port}`))
+
+
+
+
+//*************For chat App**************** */
+// const express = require('express')
+// const Sse = require('json-sse')
+// const bodyParser = require('body-parser')
+
+// const data = 'hello world'
+
+// const sse = new Sse(data)
+
+// const app = express()
+
+// const jsonParser = bodyParser.json()
+// app.use(jsonParser)
+
+// app.get('/stream', sse.init)
+
+// app.post('/message', (req,res) => {
+//   const { message } = req.body
+//   sse.send(message)
+//   res.send(message)
+// })
+
+// const port = process.env.PORT || 5000
+
+// app.listen(port, () => console.log(`Listning on ${port}`))
